48e3a82677d7afb5c607ceba387a95e39e1f393c
Flu3nt <gnedyalkov94@gmail.com>
2023-08-04 09:15:46 +0300

    initialize repo and setup tailwind

---
+++ ts_client/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <>
+            <header className="h-10">
+                <h1 className="text-3xl font-bold text-center">
+                    Real Estate Buy and Hold Calculator
+                </h1>
+            </header>
+        </>
+    );
+}
+
+export default App;
