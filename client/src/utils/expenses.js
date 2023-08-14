1193679a72ef67d70a1d587b78ce2294d7dd64fe
Flu3nt <gnedyalkov94@gmail.com>
2023-08-14 12:12:06 +0300

    refactor remove comments

--- client/src/utils/expenses.js
+++ client/src/utils/expenses.js
@@ -2,38 +2,18 @@ function calculateExpenses(expenses, monthlyRentalIncome) {
     var sum = 0;
     for (var key in expenses) {
         console.log(key);
-        if (key === "repairsAndMaintenance" ||
+        if (
+            key === "repairsAndMaintenance" ||
             key === "vacancy" ||
             key === "capitalExpenditures" ||
-            key === "managementFees") {
+            key === "managementFees"
+        ) {
             sum += (expenses[key] / 100) * monthlyRentalIncome;
-        }
-        else {
+        } else {
             sum += expenses[key];
         }
     }
     return sum;
-    // const repairsAndMaintenanceCost =
-    //     (expenses.repairsAndMaintenance / 100) * monthlyRentalIncome;
-    // const vacancyCost = (expenses.vacancy / 100) * monthlyRentalIncome;
-    // const capitalExpenditureCost =
-    //     (expenses.capitalExpenditures / 100) * monthlyRentalIncome;
-    // const managementFeesCost =
-    //     (expenses.capitalExpenditures / 100) * monthlyRentalIncome;
-    // const totalExpenses =
-    //     expenses.propertyTaxes +
-    //     expenses.insurance +
-    //     repairsAndMaintenanceCost +
-    //     vacancyCost +
-    //     capitalExpenditureCost +
-    //     managementFeesCost +
-    //     expenses.electricity +
-    //     expenses.gas +
-    //     expenses.water +
-    //     expenses.HOAfees +
-    //     expenses.garbage +
-    //     expenses.other;
-    // return totalExpenses;
 }
 var totalExpenses = {
     propertyTaxes: 10,
