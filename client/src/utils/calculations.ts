24d395f10931013dd0a3782c8eb2da37eaed390a
Flu3nt <gnedyalkov94@gmail.com>
2023-08-22 13:27:20 +0300

    feature add script to calculate line chart data for KPI results

--- client/src/utils/calculations.ts
+++ client/src/utils/calculations.ts
@@ -22,3 +22,40 @@ export function calculateRentToPropertyValueRatio(
 ): number {
     return (rent / propertyValue) * 100;
 }
+
+export function calculateLineChartData(
+    expenses: number,
+    rentalIncome: number,
+    annualIncomeGrowthPercentage: number,
+    annualExpensesGrowthPercentage: number,
+    cashInvested: number
+) {
+    const results = [];
+    let years = 1;
+
+    while (years <= 30) {
+        const cashFlow = rentalIncome - expenses;
+
+        const currentYear = {
+            name: `Year ${years}`,
+            cashFlow,
+            expenses,
+            rentalIncome,
+            return: ((cashFlow / cashInvested) * 100).toFixed(2),
+        };
+
+        console.log(currentYear);
+
+        results.push(currentYear);
+
+        years++;
+
+        const rentalIncomeGrowthPercentage = annualIncomeGrowthPercentage / 100;
+        const expensesGrowthPercentage = annualExpensesGrowthPercentage / 100;
+
+        expenses += Math.round(expensesGrowthPercentage * expenses);
+        rentalIncome += Math.round(rentalIncomeGrowthPercentage * rentalIncome);
+    }
+
+    return results;
+}
